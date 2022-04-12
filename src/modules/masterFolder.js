c49e26740d48c64286813321b2ff55fac13020a3
Emily <emilydta@users.noreply.github.com>
2022-04-12 20:21:48 +1000

    refactor: add master folder file

---
+++ src/modules/masterFolder.js
@@ -0,0 +1,53 @@
+const masterFolder = [{
+    folderName: "All Todos",
+    todos: [{
+        Title: "thing",
+        Description: "desciprwf",
+        Priority: "High",
+        Date: "",
+        Folder: "",
+        idNumber: "123"
+    },
+    {
+        Title: "bloop",
+        Description: "desciprwf",
+        Priority: "Low",
+        Date: "",
+        Folder: "",
+        idNumber: "124"
+    },
+    {
+        Title: "sdfp",
+        Description: "desciprwf",
+        Priority: "Low",
+        Date: "",
+        Folder: "",
+        idNumber: "127"
+    },
+    ],
+    idNumber: "all-todos"
+},
+{
+    folderName: "Project",
+    todos: [{
+        Title: "thing",
+        Description: "desciprwf",
+        Priority: "High",
+        Date: "",
+        Folder: "",
+        idNumber: "123"
+    },
+    {
+        Title: "bloop",
+        Description: "desciprwf",
+        Priority: "Low",
+        Date: "",
+        Folder: "",
+        idNumber: "124"
+    },
+    ],
+    idNumber: "project"
+},
+];
+
+export { masterFolder };
