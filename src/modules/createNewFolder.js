0db2c0182109de8e4bdfab21555afca4fab36386
Emily <emilydta@users.noreply.github.com>
2022-04-16 18:07:44 +1000

    refactor: tidied file

--- src/modules/createNewFolder.js
+++ src/modules/createNewFolder.js
@@ -1,8 +1,5 @@
 import { todoFunctions } from "./todoFunctions";
 import { masterFolder } from "./masterFolder";
-import { createDisplay } from "./interface";
-
-const display = createDisplay();
 
 const newFolderTemplate = (folderName, todos, idNumber) => {
     return { folderName, todos, idNumber }
@@ -12,11 +9,11 @@ const createNewFolder = (newFolder) => {
     const useFunction = todoFunctions();
     const folderNameInput = document.getElementById('new-folder-input');
     if (!folderNameInput.value) {
-        return console.log("error") //add function to highlight empty field;
+        return 
     } else 
     newFolder = newFolderTemplate(`${folderNameInput.value}`, [], useFunction.createIdNumber());
     useFunction.addToMasterFolder(newFolder, masterFolder);
-    display.createNewFolderListing(newFolder);
+    return { newFolder } 
 }
 
 export { createNewFolder };
--- src/modules/createNewTodo.js
+++ src/modules/createNewTodo.js
@@ -1,4 +1,3 @@
-import { format } from 'date-fns';
 import { todoFunctions } from "./todoFunctions";
 import { masterFolder } from './masterFolder';
 
@@ -8,37 +7,21 @@ const newTodoTemplate = (Title, Description, Priority, Date, Folder, idNumber) =
 
 const createNewTodo = (newTodo) => { 
     const useFunction = todoFunctions();
-    const titleInput = document.getElementById('title');
-    const descriptionInput = document.getElementById('description');
-    const priorityDropdown = document.getElementById('priority-dropdown');
-    const dateInput = document.getElementById('date-input');
-    const timeInput = document.getElementById('time-input');
-
-    console.log("isthsi")
-    
-    const transformDateDisplay = (dateData) => {
-        dateData = dateData.replace(/-/g, ", ");
-        return format(new Date(dateData), 'dd/MMM/yyyy');
-    }
-    const findFolderName = (mainFolder) => {
-        let folder = '';
-        const folderDropdown = document.getElementById('folder-dropdown');
-        for (let i = 0; i < mainFolder.length; i++) { 
-            if (mainFolder[i].idNumber == folderDropdown.value) {
-                folder = `${mainFolder[i].folderName}`
-            }
-        }
-        return folder;
-    }
-    if (!titleInput.value) {
-        return console.log('error')//add function to highlight empty field;
+    if (!useFunction.titleInput.value) {
+        return alert ("Please add a title for your todo.")
     } else {
-        newTodo = newTodoTemplate(`${titleInput.value}`, `${descriptionInput.value}`, `${priorityDropdown.value}`, `${transformDateDisplay(dateInput.value)} ${timeInput.value}`, `${findFolderName(masterFolder)}`, `${useFunction.createIdNumber()}`);
+        newTodo = newTodoTemplate(
+            `${useFunction.titleInput.value}`, 
+            `${useFunction.descriptionInput.value}`, 
+            `${useFunction.priorityDropdown.value}`, 
+            `${useFunction.transformDateDisplay(useFunction.dateInput.value)} ${useFunction.timeInput.value}`, 
+            `${useFunction.findFolderName(masterFolder)}`, `${useFunction.createIdNumber()}`
+            );
         useFunction.addToFolders(newTodo, masterFolder);
     }
 }
 
-export { createNewTodo }
+export { createNewTodo, newTodoTemplate }
 
 
 
